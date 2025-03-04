7a76e6cf7aeb2d3747f7ceba56ec46bf57b0ecb8
Arya gunawan <147965614+Aryaagunawan@users.noreply.github.com>
2025-03-05 00:55:25 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -8,7 +8,7 @@ st.set_page_config(
     page_title="Dashboard",
     page_icon="📝",
     layout="wide"
-
+)
 # Load dataset
 file_path = "https://raw.githubusercontent.com/Aryaagunawan/Proyek-Analisis-Data/refs/heads/master/dashboard/PRSA_Data_Dingling_20130301-20170228.csv"
 df = pd.read_csv(file_path, on_bad_lines='skip')
